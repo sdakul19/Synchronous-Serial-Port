4f0bf1eb999cf62bafbcbfa0f5cad729f85cd07b
Sam Rojanasakdakul <143771553+sdakul19@users.noreply.github.com>
2024-01-17 15:46:30 -0500

    Update SSP_tb.v

--- SSP_tb.v
+++ SSP_tb.v
@@ -1,21 +1,27 @@
+/*
+ * Author: Sam Rojanasakdakul
+ * 
+ * Description: A testbench for the non-looped synchronous serial port where the receive FIFO and transmit FIFO are not connected
+ *		It tests the parallel to serial conversion through the SSP by defining the pwdata.
+ */
 module SSP_tb();						//Testing transmitting data
 
-	reg pclk;
-	reg clear_b;
-	reg psel;
-	reg pwrite;
-	reg sspfssin;
-	reg ssprxd;
-	reg [7:0] pwdata;
-	reg sspclkin;
+	reg pclk;			//Clock for Serial Synchronous Port
+	reg clear_b;			//Low active clear signal
+	reg psel;			//Chip select signal
+	reg pwrite;			//Write/Read signal
+	reg sspfssin;			//Frame control signal for reception
+	reg ssprxd;			//Serial data input
+	reg [7:0] pwdata;		//8-bit transmission data
+	reg sspclkin;			//Synchronization clock for receive
 
-	wire ssptxintr;
-	wire ssprxintr;
-	wire ssptxd;
-	wire sspoe_b;
-	wire sspfssout;
-	wire [7:0] prdata;
-	wire sspclkout;
+	wire ssptxintr;			//Transmission interrupt signal
+	wire ssprxintr;			//Receive interrupt signal
+	wire ssptxd;			//Serial data output
+	wire sspoe_b;			//Active low output enable signal
+	wire sspfssout;			//Frame control signal for transmission
+	wire [7:0] prdata;		//8-bit receive data
+	wire sspclkout;			//Synchronization clock for transmission
 
 	SSP SSP_instance(
 		.pclk(pclk),
@@ -37,7 +43,7 @@ module SSP_tb();						//Testing transmitting data
 
 	always #5 pclk = ~pclk;
 
-	initial begin
+	initial begin				//initializes SSP
 		pclk = 0;
 		sspclkin = 0;
 		sspfssin = 0;
@@ -46,30 +52,28 @@ module SSP_tb();						//Testing transmitting data
 		psel = 1;
 		pwdata = 8'hFF;
 		
-		#10
+		#10				//Preparing SSP to write
 		clear_b = 1;
 		pwrite = 1;
 		pwdata = 8'h0F;
 		
 		
-		#10
-		clear_b = 1;
-		pwrite = 1;
+		#10				//Stores 8'b10101010 to transmit fifo	
 		pwdata = 8'b10101010;
 		
-		#10
+		#10				//Stores 8'b11110000 to transmit fifo and serial outputs 8'b10101010
 		pwdata = 8'b11110000;
 
-		#10
+		#10				//Stores 8'b01010101 to transmit fifo and serial outputs 8'b11110000
 		pwdata = 8'b01010101;
 
-		#10
-		pwdata = 8'b00001111;
+		#10				//Stores 8'b00001111 to transmit fifo and serial outputs 8'b01010101
+		pwdata = 8'b00001111;	
 		
-		#10
+		#10				//Stores 8'b11111111 to transmit fifo and serial outputs 8'b00001111
 		pwdata = 8'b11111111;
 
-		#10
+		#10				//Serial outputs 8'b11111111 and deactivates SSP
 		psel = 0;
 		
 	end
