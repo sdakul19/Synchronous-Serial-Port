a749941f12a4bfd78888d891560cfcd540134ea0
Sam Rojanasakdakul <143771553+sdakul19@users.noreply.github.com>
2024-01-17 14:57:50 -0500

    Update RxFIFO_tb.v

--- RxFIFO_tb.v
+++ RxFIFO_tb.v
@@ -1,14 +1,20 @@
+/*
+ * Author: Sam Rojanasakdakul
+ * 
+ * Description: Test bench module of the receive FIFO
+ *
+ */
 module RxFIFO_tb();
 
-	reg pclk;
-	reg clear_b;
-	reg psel;
-	reg pwrite;
-	reg w_en;
-	reg [7:0] rxdata;
+	reg pclk;			//Clock for synchronous serial port
+	reg clear_b;			//Low active clear signal
+	reg psel;			//Chip select signal
+	reg pwrite;			//Read signal when pwrite = 0
+	reg w_en;			//Write enable to the fifo
+	reg [7:0] rxdata;		//8 bit receive data input into the FIFO
 	
-	wire ssprxintr;
-	wire [7:0] prdata;
+	wire ssprxintr;			//Interrupt signal if FIFO is full
+	wire [7:0] prdata;		//8 bit receive data being read out
 
 	RxFIFO RxFIFO_instance(
 		.pclk(pclk),
@@ -23,7 +29,7 @@ module RxFIFO_tb();
 
 	always #5 pclk = ~pclk;
     
-    	initial begin
+    	initial begin			//Initializes data
         	pclk = 0;
         	clear_b = 0;
         	psel = 1;
@@ -31,25 +37,25 @@ module RxFIFO_tb();
         	w_en = 1;
         	rxdata = 8'h00;
         
-        	#10
+        	#10			//Stores 8'h00 in first lcoation
         	clear_b = 1;
 		psel = 0;
 		pwrite = 0;
         	rxdata = 8'h00;
         
-        	#10    
+        	#10    			//Stores 8'h01 in second location
         	rxdata = 8'h01;
         
-        	#10 
+        	#10 			//Stores 8'h02 in third location
         	rxdata = 8'h02;
         
-        	#10
+        	#10			//Stores 8'h03 in fourth location
         	rxdata = 8'h03;
         
-        	#10
+        	#10			//Fails to store 8'h04 because FIFO is full
         	rxdata = 8'h04;
 
-		#10
+		#10			//Receive FIFO is activated and data should be read off from FIFO
 		psel = 1;
 		 
     	end
