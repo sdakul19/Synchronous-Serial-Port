ef593ab0abaf4710f12f10f371ed026c315613d1
Sam Rojanasakdakul <143771553+sdakul19@users.noreply.github.com>
2024-01-18 13:52:24 -0500

    Update SSP_tb2.v

--- SSP_tb2.v
+++ SSP_tb2.v
@@ -1,18 +1,24 @@
+/*
+ * Author: Sam Rojanasakdakul
+ * 
+ * Description: A testbench for the looped synchronous serial port where the receive FIFO and transmit FIFO are connected.
+ *		The serial output of the transmit FIFO becomes the serial input of the receive FIFO.
+ */
 module SSP_tb2();					//Testing looped SSP with transmit wire to receive
 
-	reg pclk;
-	reg clear_b;
-	reg psel;
-	reg pwrite;
-	reg [7:0] pwdata;
+	reg pclk;					//Clock for Serial Synchronous Port
+	reg clear_b;					//Low active clear signal
+	reg psel;					//Chip select signal
+	reg pwrite;					//Write/Read signal
+	reg [7:0] pwdata;				//8-bit transmission data
 
-	wire [7:0] prdata;
-	wire sspoe_b;
+	wire [7:0] prdata;				//8-bit receive data
+	wire sspoe_b;					//Active low output enable signal
 	wire tx_to_rx;					//Serial transmit output data from SSP enters as serial receive input data
 	wire clk_wire;					//sspclkout wires to sspclkin
 	wire fss_wire;					//sspfssout wires to sspfssin
-	wire ssptxintr;
-	wire ssprxintr;
+	wire ssptxintr;					//Transmission interrupt signal
+	wire ssprxintr;					//Receive interrupt signal
 	
 	always #5 pclk = ~pclk;
 	
@@ -44,7 +50,7 @@ module SSP_tb2();					//Testing looped SSP with transmit wire to receive
 		#10
 		clear_b = 1;
 		pwrite = 1;
-		pwdata = 8'h51;
+		pwdata = 8'h51; 
 
 		#10
 		pwdata = 8'h24;
